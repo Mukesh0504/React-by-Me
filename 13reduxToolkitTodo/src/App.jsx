001dee00e8c6f1085686ea980444183600b85418
Mukesh Dwivedi <rahuldwivedi486001@gmail.com>
2025-03-21 19:18:44 +0530

    todo is almost completed

--- 13reduxToolkitTodo/src/App.jsx
+++ 13reduxToolkitTodo/src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
-
+import AddTodo from './components/addTodo'
+import Todo from './components/Todo'
 function App() {
 
   return (
     <>
       <h1>Mukesh is here</h1>
+      <AddTodo />
+      <Todo />
     </>
   )
 }
