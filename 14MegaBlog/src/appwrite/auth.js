6ba24e9648a60793d59096617e26cc6af3d903d5
Mukesh Dwivedi <rahuldwivedi486001@gmail.com>
2025-03-29 04:15:19 +0530

    creating authentication

---
+++ 14MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,9 @@
+import conf from '../conf/conf'
+import { client, Account, ID } from "appwrite"
+
+
+export class AuthService { }
+
+const authService = new AuthService();
+
+export default AuthService;
