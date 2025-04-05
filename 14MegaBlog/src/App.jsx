d656c56d2ccebf1e6f45eb498693f7467fa90c8d
Mukesh Dwivedi <rahuldwivedi486001@gmail.com>
2025-04-06 03:12:09 +0530

    components added

--- 14MegaBlog/src/App.jsx
+++ 14MegaBlog/src/App.jsx
@@ -1,17 +1,40 @@
+import { useState, useEffect } from 'react'
 import './App.css'
-
+import { useDispatch } from 'react-redux'
+import authservice from '../src/appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from '../src/components/index'
 function App() {
 
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
 
 
-  return (
-    <>
-      <h1>A Blog App with Appwrite
-      </h1>
-    </>
-  )
+  useEffect(() => {
+    authservice.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      }).finally(() => setLoading(false))
+  }, [])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO:  {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
