925fc425f84f2843c8ddfec0c140d1db1e9d8f25
Mukesh Dwivedi <rahuldwivedi486001@gmail.com>
2025-04-20 03:19:34 +0530

    postcard added

--- 14MegaBlog/src/components/Input.jsx
+++ 14MegaBlog/src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = React.forwardRef(function Input(
     { label, type = "text", className = "", ...props },
     ref
 ) {
-    const id = useId()
+    const id = useId();
 
     return (
         <div className="w-full">
@@ -13,7 +13,8 @@ const Input = React.forwardRef(function Input(
                     {label}
                 </label>
             )}
-            <input type={type}
+            <input
+                type={type}
                 className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-200 w-full  ${className}`}
                 ref={ref}
                 {...props}
