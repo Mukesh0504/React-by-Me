90dcc3ebbc2dfbd1e3f4e0238e299cfef0ebb5ce
Mukesh Dwivedi <rahuldwivedi486001@gmail.com>
2025-04-24 05:10:33 +0530

    signup page added

--- 14MegaBlog/src/components/index.js
+++ 14MegaBlog/src/components/index.js
@@ -3,6 +3,9 @@ import Footer from './Footer/Footer';
 import Container from './container/Container'
 import Logo from './Logo'
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Select from './Select';
+import Input from './Input'
 
 
-export { Header, Footer, Container, Logo, LogoutBtn } 
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Select, Input } 
